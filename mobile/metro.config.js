cf33b2d8cb2f598a3b30a4a9a515e68bfe6880f1
kaleb <kaleb.online@outlook.com>
2025-11-17 00:33:51 -0800

    chore(mobile): update build config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,6 +4,28 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = true; 
+config.resolver.unstable_enablePackageExports = true;
+
+// Optimize asset resolution for smaller bundle size
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+// Enable minification in production
+config.transformer = {
+  ...config.transformer,
+  minifierConfig: {
+    keep_classnames: false,
+    keep_fnames: false,
+    mangle: {
+      keep_classnames: false,
+      keep_fnames: false,
+    },
+    compress: {
+      drop_console: true, // Remove console.logs in production
+      drop_debugger: true,
+      pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
+    },
+  },
+};
 
 module.exports = config;
